34063a4c6e3ce8bcd320466d57db2ce23b98cba5
LukasC0D <kaupasa666@gmail.com>
2022-09-25 16:37:36 +0300

    Update on README.md

---
+++ MySQL database.sql
@@ -0,0 +1,103 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: projectsdb
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+INSERT INTO `people` VALUES (1,'Arturas','Bartkus'),(2,'Simonas','Vaitkus'),(3,'Alfredas ','Zvonkus'),(4,'Danute','Neveronyte'),(5,'Jurgita','Pareigyte'),(6,'Aiste','Mickaite');
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'HTML'),(2,'Css'),(3,'JavaScript'),(4,'React'),(5,'PhP'),(6,'MySQL'),(7,'Laravel');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects_people`
+--
+
+DROP TABLE IF EXISTS `projects_people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects_people` (
+  `prj_id` int(11) DEFAULT NULL,
+  `pers_id` int(11) DEFAULT NULL,
+  KEY `prj_id` (`prj_id`),
+  KEY `pers_id` (`pers_id`),
+  CONSTRAINT `projects_people_ibfk_1` FOREIGN KEY (`prj_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `projects_people_ibfk_2` FOREIGN KEY (`pers_id`) REFERENCES `people` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects_people`
+--
+
+LOCK TABLES `projects_people` WRITE;
+/*!40000 ALTER TABLE `projects_people` DISABLE KEYS */;
+INSERT INTO `projects_people` VALUES (7,2),(2,5),(4,1),(6,1),(3,2),(3,6),(5,3),(3,4),(1,3),(1,6);
+/*!40000 ALTER TABLE `projects_people` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-25 16:19:02
---
+++ README.md
@@ -0,0 +1,50 @@
+### Welcome to Practical Project 7
+
+## Product name 
+
+Project manager with MySQL database.
+
+## With a project manager you can:
+
+- Create a new project. 
+- Delete a project.
+- Edit a projects 
+- Add a new person.
+- Add a multiple persons to a project.
+- Delete a person 
+- Edit a person
+
+## How to setup MySQL server?
+
+- MySQL database file is atached into the project.
+- Go to MySQL workbench and create new schema called "projectsdb".
+- Import MySQL database file to a new schema you just created.
+- If everything  succeeded you can go to next step.
+
+## How to run?
+
+- Download [XAMPP](https://www.apachefriends.org/index.html) and install it.
+- Clone or download git repository https://github.com/LukasC0D/php_file_browser. 
+- Add repository to htdocs.The folder is located in the C drive (C:/xampp/htdocs).
+- Or clone repository directly to htdocs. In htdocs directory right mouse click Git Bash Here. Type in:
+```sh
+git clone https://github.com/LukasC0D/php_prj_ppl.git
+```
+- Open cloned repository with VS Code. Optional but not necessary. You can already.
+- Run XAMPP. In the C drive run (C:/xampp/xampp-control.exe) and start the Apache server.
+- If the Apache server strated successfully open Your browser.
+- In Your browser on the searchbar type in:
+
+```sh
+localhost/php_prj_ppl
+```
+
+## What technologies I used?
+
+ Raw PHP, MySQL, CSS with BOOTSTRAP.
+
+
+
+
+
+
